221a64eb465c1d966e24d9ca5a3ec3262b00ef29
Taylor Williams <taylor@thewilliams.net>
2018-05-30 21:36:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,6 +10,7 @@ class Song
     split = file.split(/ - /)
     new_song = Song.new(split[1])
     new_song.artist = Artist.find_or_create_by_name(split[0])
+    new_song.artist.add_song(self)
     new_song
   end
 end
